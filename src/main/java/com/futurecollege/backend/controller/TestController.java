18a6de3a8bb3b83ab750ed4e5ffa58b1732887fa
nageshchillale <nageshchillale18@gmail.com>
2025-06-24 08:13:20 +0530

    Prepare for Render deployment

---
+++ src/main/java/com/futurecollege/backend/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.futurecollege.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String testEndpoint() {
+        return "Backend is working!";
+    }
+}
